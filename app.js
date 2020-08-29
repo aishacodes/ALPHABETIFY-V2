f91d73e4ca045973f80527b4b0464aa7721fefbd
Aishat Abdulfatah <aishatabdulfatah81@gmail.com>
2020-08-29 16:10:49 +0100

    feat: alphabetify

---
+++ app.js
@@ -0,0 +1,48 @@
+let words = { a: 'Apple', b: 'Baseball', c: 'Clock', d: 'Donkey', e: 'Elephant', f: 'F' }
+const wordsEl = document.querySelector('.words')
+const letterEls = Array.from(document.querySelectorAll('span[data-key]'))
+
+function playsound(e) {
+    let key;
+    if (e.type === 'keydown') {
+        key = e.key.toLowerCase();
+    } else if (e.type === 'click') {
+        key = e.target.dataset.key
+    }
+
+    if (!key) return
+
+    const letter = document.querySelector(`span[data-key=${key}]`)
+    const audio = document.querySelector(`audio[data-key=${key}]`)
+
+    letter.classList.add('active')
+    removeActiveClass(key)
+    audio.currentTime = 0
+    audio.play()
+
+    const word = words[key]
+    console.log(word)
+    if (!word) return
+
+    wordsEl.innerHTML = ''
+
+    word.split('').forEach(l => {
+        wordsEl.innerHTML += `<span>${l}</span>`
+    })
+}
+
+function removeActiveClass(key, timeout = 1000) {
+    setTimeout(() => {
+        document.querySelector(`span[data-key=${key}]`).classList.remove('active')
+    }, timeout)
+}
+
+letterEls.forEach(letter => {
+    letter.addEventListener('click', playsound)
+    letter.addEventListener('transitionend', (e) => {
+        console.log(e)
+    })
+})
+
+window.addEventListener('keydown', playsound)
+    // window.addEventListener('click', playsoundd)
